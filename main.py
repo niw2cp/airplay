af14a73ac9f61ca47a494ffe1f93e5688620136b
FezFerrari <32849182+FezFerrari@users.noreply.github.com>
2018-04-20 14:48:19 -0400

    Text Switch
    If text file selected send to readfile()

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ import os, random
 played = 1
 
 def rndmp3 ():
-   global played
-   randomfile = random.choice(os.listdir("/home/pi/Music/"))
-   file = ' /home/pi/Mainlist/'+ randomfile
-   if file == played:
-    pass
-   else:
-    print file
-    os.system ('omxplayer -o local' + file)    
-    played = file
+    global played
+    randomfile = random.choice(os.listdir("OnAir/"))
+    file = ' /home/pi/Mainlist/'+ randomfile
+    if file == played:
+        pass
+    
+    elif randomfile.endswith('.txt'):
+        readfile(file)
+        #print(randomfile)
+            
+        
+        
+    
+    else:
+        print(randomfile)
+        os.system ('omxplayer -o local' + file)    
+        played = file
 
 while True:
- rndmp3 ()
+    rndmp3 ()
 fi
 done
