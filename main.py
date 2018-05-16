339cabea696346a890202b4b46227c1964740250
EL-PATRON-777 <38295809+EL-PATRON-777@users.noreply.github.com>
2018-05-16 13:52:49 -0400

    Adding weather

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 import os, random
+from lxml import etree
 
 played = 1
+def read_forecast():
+   file = open('forecast.xml')
+   tree = etree.parse(file)
+   timename = tree.xpath('//data/time-layout/start-valid-time/@period-name')
+   cast = tree.xpath('//wordedForecast/text/text()')
+   forecast = timename[0],"it's", cast[0], timename[1], "will be", cast[1]
+   engine = pyttsx3.init()
+   engine.say(forecast)
+   engine.runAndWait()
 
 def rndmp3 ():
    global played
